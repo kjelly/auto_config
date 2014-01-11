bf0f20343de14c78bd844f1fd7e0d758e5e84b8b
ya790206 <ya790206@debian>
2014-01-11 18:16:48 +0800

    add install package script.

---
+++ install_package.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+import os
+
+
+def sudo(cmd):
+    os.system("sudo %s" % cmd)
+
+
+def apt_get_install(package):
+    cmd = "apt-get -y install %s" % package
+    sudo(cmd)
+
+
+apt_get_install("build-essential")
+apt_get_install("python-dev")
+apt_get_install("wireless-tools")
