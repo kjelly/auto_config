886ea75612acb109a3776f5e6e432fefb5c7250f
ya790206 <ya790206@debian>
2014-01-17 00:46:22 +0800

    control dual monitor scipt.

---
+++ dual_screen.sh
@@ -0,0 +1,5 @@
+xrandr --output VGA1 --mode 1920x1080
+xrandr --output LVDS1 --mode 1366x768
+xrandr --output LVDS1 --right-of VGA1
+feh --bg-scale ~/Picture/background.jpg
+
---
+++ turn_off.sh
@@ -0,0 +1,3 @@
+xrandr --output LVDS1 --off
+xrandr --output VGA1 --mode 1920x1080
+
