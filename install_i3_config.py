a2187138fcedaa08eeb8b4830777a283075d04d8
ya790206 <ya790206@gmail.com>
2014-01-18 21:01:04 +0800

    add script to install_i3_config for installing dmenu, py3status.

--- install_i3_config.py
+++ install_i3_config.py
@@ -9,7 +9,11 @@ config_parent_path = os.path.dirname(__file__)
 config_path = os.path.join(config_parent_path, 'i3')
 make_link(config_path, target)
 
+os.system("sudo apt-get install i3 i3status")
 os.system("sudo apt-get install terminator")
+os.system("sudo apt-get install python-pip")
+os.system("sudo apt-get install dmenu")
+os.system("sudo pip install py3status")
 
 print 'Please install terminator or change terminal value in .i3/config.'
 print 'Please install dmenu.'
