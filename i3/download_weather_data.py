9164c50d60bcac8a878578bfa98c44623b4b825d
ya790206 <ya790206@gmail.com>
2014-02-16 11:47:56 +0800

    Fix bug. auto create .i3/data folder if the target folder doesn't exist.

--- i3/download_weather_data.py
+++ i3/download_weather_data.py
@@ -4,7 +4,11 @@ import os.path
 from ftplib import FTP
 
 i3_user_config_path = os.path.join(os.path.expanduser('~'),  '.i3')
-weather_data_path = os.path.join(i3_user_config_path,        'data',  'W002.txt')
+i3_data_path = os.path.join(i3_user_config_path,'data')
+if not os.path.exists(i3_data_path):
+    os.mkdir(i3_data_path)
+
+weather_data_path = os.path.join(i3_data_path,  'W002.txt')
 
 ftp = FTP('ftpsv.cwb.gov.tw')
 ftp.login()
