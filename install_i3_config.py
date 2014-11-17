c2416c6e7c667c91b1556e958e697a137e0f74e1
ya790206 <ya790206@gmail.com>
2014-11-17 22:57:55 +0800

    [i3] use win_l and caps_lock as Mod key

--- install_i3_config.py
+++ install_i3_config.py
@@ -5,9 +5,10 @@ from os import listdir
 
 target = os.path.join(get_home_dir(), '.i3')
 
-config_parent_path = os.path.dirname(__file__)
+config_parent_path = os.path.abspath(os.path.dirname(__file__))
 config_path = os.path.join(config_parent_path, 'i3')
 make_link(config_path, target)
+make_link('%s/Xmodmap' % config_parent_path, '%s/.Xmodmap' % get_home_dir())
 
 os.system("sudo apt-get -y install i3 i3status")
 os.system("sudo apt-get -y install terminator")
