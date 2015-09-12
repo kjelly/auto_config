195f717074531e0106ebf22cfad0a6f9173f85b8
Kuo-tung Kao <ya790206@gmail.com>
2015-09-12 13:10:02 +0800

    update dual_screen and run dual_screen when i3 started.

--- roles/i3/files/i3-config/dual_screen.sh
+++ roles/i3/files/i3-config/dual_screen.sh
@@ -1,9 +1,3 @@
 #! /usr/bin/env bash
-screen1="DVI-I-0"
-screen2="DP-0"
-xrandr --output $screen1 --mode 1920x1080
-xrandr --output $screen2 --mode 1440x900
-xrandr --output $screen2 --left-of $screen1
-#xrandr --output $screen1 --primary
-feh --bg-scale ~/Pictures/background.jpg
-
+python ~/.i3/auto_config
+python ~/.i3/new_screen_in.py left
