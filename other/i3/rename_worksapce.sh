4e8629c230d043e9fea51bdb8c31aee25febf445
Kuo-tung Kao <ya790206@gmail.com>
2016-01-11 21:56:33 +0800

    remove old thing
    Former-commit-id: bcbf55674f9e45b9b86f182563d38fe61851b050 [formerly d83cf5d2f5fe257f3069d688510e395ad44078ac]
    Former-commit-id: 96ae8173c70e0e616752017eecc8abe1b5b9dcb4

--- other/i3/rename_worksapce.sh
+++
@@ -1,2 +0,0 @@
-#! /usr/bin/env bash
-i3-msg "rename workspace $1 to $2"
--- other/i3/swap_workspace_with_current.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-import json
-from lib import get_active_workspace, get_first_args, swap_workspace
-
-workspace = get_active_workspace()
-swap_workspace(workspace['num'], int(get_first_args()))
