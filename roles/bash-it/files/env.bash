f663d6a0ee6b73c1110719987f805994aeab1a9f
Kuo-tung Kao <ya790206@gmail.com>
2016-01-16 17:07:01 +0800

    add bash-it role
    Former-commit-id: 80454f5946dc783f34f7c239571cf7d9b2253292

---
+++ roles/bash-it/files/env.bash
@@ -0,0 +1,8 @@
+alias tclsh='rlwrap -c tclsh8.6'
+export PATH=$PATH:/usr/local/go/bin
+export GOROOT=$HOME/go
+export PATH=$PATH:$GOROOT/bin:/home/user/github/swift/bin
+
+export PATH="`pwd`/mybin/:$PATH"
+source ~/.local_bashrc
+
