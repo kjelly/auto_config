29317d2035bda956d3e93e4b6a740e712ca75269
Kuo-tung Kao <ya790206@gmail.com>
2016-01-23 19:41:00 +0800

    install deb dependency without asking
    Former-commit-id: 830094eb49850fe8799809f9256e4a55041273e1

--- install-deb-dependency.sh
+++ install-deb-dependency.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-sudo apt-get install python-dev git python-pip
-sudo apt-get build-dep ansible
+sudo apt-get install -y python-dev git python-pip
+sudo apt-get build-dep -y ansible
 sudo pip install ansible
