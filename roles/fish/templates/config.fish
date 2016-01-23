8c7b20c1b870822c2dd247b5217dfc22760eb9d8
Kuo-tung Kao <ya790206@gmail.com>
2016-01-23 17:30:23 +0800

    add ~/bin and ~/mybin to path in config.fish
    Former-commit-id: 954041102bc00dc8e288b56606fa7aa4cbc3e542 [formerly 9babdc7805f2df5340fb5ad1403591d88ae0fea5]
    Former-commit-id: 094b0c82c161c2d8deb6c501aa5bf3046b12f15a

--- roles/fish/templates/config.fish
+++ roles/fish/templates/config.fish
@@ -7,4 +7,6 @@ set -gx OMF_PATH "{{ HOME_PATH }}/.local/share/omf"
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
 
+set -gx PATH  "{{ HOME_PATH }}/bin"  "{{ HOME_PATH }}/mybin" $PATH
+
 eval (python -m virtualfish)
