a87f0e93a96c7cbe4fdafca28b24ff191acdec55
Kuo-tung Kao <ya790206@gmail.com>
2016-02-18 22:12:27 +0800

    use fisher instead of oh-my-fish
    Former-commit-id: 74e6804b5999248e749d068609d260f3a61f869c [formerly 57ee7888c84dbbe66e46aa048a6432e35c32b3bb]
    Former-commit-id: 3e620e5666f6a0c64338d3ea77c1990e4fe7e8c7

--- roles/fish/templates/config.fish
+++ roles/fish/templates/config.fish
@@ -1,11 +1,7 @@
-# Path to Oh My Fish install.
-set -gx OMF_PATH "{{ HOME_PATH }}/.local/share/omf"
+set fisher_home ~/.local/share/fisherman
+set fisher_config ~/.config/fisherman
+source $fisher_home/config.fish
 
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG "/home/kjelly/.config/omf"
-
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
 
 set -gx PATH  "{{ HOME_PATH }}/bin"  "{{ HOME_PATH }}/mybin" $PATH
 
