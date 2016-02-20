476775f397891e1f6e7f15d403446dafbed94de1
Kuo-tung Kao <ya790206@gmail.com>
2016-02-21 00:51:40 +0800

    record workspace name history
    Former-commit-id: ab0dae05e501ea680ad8299819e00012230fed76 [formerly 7570106f08207ba072b6a43da43283bbc34208cd]
    Former-commit-id: b62b2cc47733d9136c5f114e3c9f3a37f1b4b8aa

--- roles/i3/files/i3-config/name_workspace.py
+++ roles/i3/files/i3-config/name_workspace.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import i3
 import subprocess
+import os
+from collections import Counter
 
 
 def dmenu(options, dmenu):
@@ -15,14 +17,31 @@ def dmenu(options, dmenu):
     return stdout.decode('utf-8').strip('\n')
 
 
+config_path = os.path.join(os.path.expanduser("~"), '.i3_workspace_name_history')
+
+
+def read_history():
+    if not os.path.exists(config_path):
+        return []
+    with open(config_path, 'r') as ftr:
+        lines = ftr.readlines()
+    return [ i[0] for i in Counter(lines).most_common()]
+
+
+def write_history(name):
+    with open(config_path, 'a+') as ftr:
+        ftr.write('%s\n' % name)
+
+
 def main():
     workspaces = i3.get_workspaces()
     for i in workspaces:
         if i['focused']:
             focused_workspace_num = i['num']
-    answer = dmenu([], 'dmenu')
+    answer = dmenu(read_history(), 'dmenu')
     if answer:
         i3.rename__workspace__to('"%d: %s"' % (focused_workspace_num, answer))
+        write_history(answer)
 
 
 if __name__ == '__main__':
