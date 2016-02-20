3d65f764a447bd8492ca194b0245729e8405f18a
Kuo-tung Kao <ya790206@gmail.com>
2016-02-20 22:39:48 +0800

    clear i3status and show tab info in i3status
    Former-commit-id: 03f1f2c195d975a2e2d83ae7b3e2af1cc13c1be0 [formerly e54d2c11ee23ff6cf25dd44fcb54c6564cbfcb9d]
    Former-commit-id: 66c5ec739c30c8d5b3324eaa2baa19307ccf34fe

--- roles/i3/files/i3-config/modules/title.py
+++ roles/i3/files/i3-config/modules/title.py
@@ -1,7 +1,56 @@
 #/usr/bin/env python3
 # coding=utf-8
+import i3
+import json
+from collections import defaultdict
 from time import time
 from subprocess import check_output
+
+
+focused = False
+focused_workspace = None
+
+
+def handle_window(window):
+    global focused
+    ret = []
+    if window['name'] is None:
+        for i in window['nodes']:
+            ret.extend(handle_window(i))
+    else:
+        ret.append({
+            'name': window['name'],
+            'id': window['window'],
+            'focused': window['focused'],
+         })
+        if window['focused']:
+            focused = True
+    return ret
+
+
+def handle_workspace(workspace):
+    global focused
+    global focused_workspace
+    ret = defaultdict(lambda: [])
+    windows = workspace['nodes']
+    name = workspace['name']
+    for i in windows:
+        ret[name].extend(handle_window(i))
+        if focused == True:
+            focused_workspace = name
+            focused = False
+    return ret
+
+
+def handle_output(output):
+    '''
+    output[1] for content
+    '''
+    ret = {}
+    workspaces = output[1]['nodes']
+    for i in workspaces:
+        ret.update(handle_workspace(i))
+    return ret
 
 
 class Py3status:
@@ -18,12 +67,38 @@ class Py3status:
         # the current user doesnt change so much, cache it good
         CACHE_TIMEOUT = 1
 
-        raw = check_output("bash -c 'xprop -id $(xdotool getactivewindow)' | egrep '^WM_NAME'",
-                           shell=True).strip()
-        fragments = raw.split('=')
-        text = '='.join(fragments[1:])[3:-1]
+        try:
 
-        # set, cache and return the output
+            tree = json.loads(check_output('i3-msg -t get_tree', shell=True).decode('utf-8'))
+            result = {}
+            for output in tree['nodes']:
+                if output['name'].startswith('__'):
+                    continue
+                result.update(handle_output(output['nodes']))
+            title_list = []
+            index = 0
+            for i in result[focused_workspace]:
+                index += 1
+                name = i['name'].strip()[:30]
+                title = name
+                if i['focused']:
+                    pos_info = '%d@%d' % (index, len(result[focused_workspace]))
+                    title_list.append('  >' + title + '<  ')
+                else:
+                    title_list.append(title)
+            text = pos_info + '|' +  '|'.join(title_list)
+        except Exception as e:
+            text = str(e)
+
         response = {'full_text': text, 'name': 'title'}
         response['cached_until'] = time() + CACHE_TIMEOUT
         return (0, response)
+
+
+def main():
+    o = Py3status()
+    print(o.title(1,1))
+
+
+if __name__ == '__main__':
+    main()
