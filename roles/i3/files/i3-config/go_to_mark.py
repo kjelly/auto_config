35a93d9a536a36502e7f87d80c98e4ee00ca31c4
Kuo-tung Kao <ya790206@gmail.com>
2016-02-21 05:22:39 +0800

    user rofi instead of dmenu(i3 role)
    Former-commit-id: 3c449c62ad375f4a16629ac2482a31d411e0ae40

--- roles/i3/files/i3-config/go_to_mark.py
+++ roles/i3/files/i3-config/go_to_mark.py
@@ -2,22 +2,12 @@
 import i3
 import os
 import subprocess
-
-def dmenu(options, dmenu):
-    '''Call dmenu with a list of options.'''
-
-    cmd = subprocess.Popen(dmenu,
-                           shell=True,
-                           stdin=subprocess.PIPE,
-                           stdout=subprocess.PIPE,
-                           stderr=subprocess.PIPE)
-    stdout, _ = cmd.communicate('\n'.join(options).encode('utf-8'))
-    return stdout.decode('utf-8').strip('\n')
+from lib import dmenu, dmenu_cmd
 
 
 if __name__ == '__main__':
     marks = i3.get_marks()
-    answer = dmenu(marks, 'dmenu -i -b -l 30')
+    answer = dmenu(marks, dmenu_cmd('select mark: '))
     if answer:
         os.system('''i3-msg '[con_mark="%s"] focus' ''' % answer)
 
