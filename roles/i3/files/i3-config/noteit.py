ca09882e0af38b9eb02f7e2ee864da91ac277934
Kuo-tung Kao <ya790206@gmail.com>
2016-02-23 21:07:47 +0800

    allowed to modify title (i3 role)
    Former-commit-id: cf40d52969fdf6dfd3a6c7e3d79650f66c1e7287

---
+++ roles/i3/files/i3-config/noteit.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import i3
+import os
+import subprocess
+from os.path import expanduser
+from lib import dmenu, dmenu_cmd
+import re
+import json
+
+
+def main():
+    wid = [i.get('window') for i in i3.filter(nodes=[]) if i.get('focused')][0]
+
+    home = expanduser("~")
+    todo_db_path = os.path.join(home, '.i3_title_todo')
+    if os.path.exists(todo_db_path):
+        with open(todo_db_path, 'r') as ftr:
+            db = json.loads(ftr.read())
+    else:
+        db = {}
+
+    answer = dmenu(db.get(wid, []), dmenu_cmd('note: ')).strip()
+
+    if answer:
+        todo_list = db.get(wid, [])
+        if answer not in todo_list:
+            todo_list.append(answer)
+        db[wid] = todo_list
+        with open(todo_db_path, 'w') as ftr:
+            ftr.write(json.dumps(db))
+        os.system('''i3-msg title_format "%class | %title | {note}" '''.format(note=answer))
+
+
+if __name__ == '__main__':
+    main()
