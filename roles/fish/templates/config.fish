c043f156d272322e6c192af66060e3fac7149d19
Kuo-tung Kao <ya790206@gmail.com>
2016-02-29 23:47:05 +0800

    add cd[1-5] alias for fish
    Former-commit-id: c3c4d0fbf3e6075603bde5ec97f49394c7ab59c1

--- roles/fish/templates/config.fish
+++ roles/fish/templates/config.fish
@@ -2,8 +2,13 @@ set fisher_home ~/.local/share/fisherman
 set fisher_config ~/.config/fisherman
 source $fisher_home/config.fish
 
-
 set -gx PATH "{{ HOME_PATH }}/gohome/bin"  "{{ HOME_PATH }}/bin"  "{{ HOME_PATH }}/mybin" $PATH
+
+alias cd1 "cd .."
+alias cd2 "cd ../.."
+alias cd3 "cd ../../.."
+alias cd4 "cd ../../../.."
+alias cd5 "cd ../../../../.."
 
 eval (python -m virtualfish)
 
