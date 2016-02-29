f6d4161ae28f4c8fbc809350b4e2261d863059fa
Kuo-tung Kao <ya790206@gmail.com>
2016-02-29 23:38:49 +0800

    update PATH env var(fish, zsh, bash, bash-it role)
    Former-commit-id: e793fa11c0e0a01ad5cf030b71aa6b3c486904fa

--- roles/bash-it/files/env.bash
+++ roles/bash-it/files/env.bash
@@ -3,6 +3,6 @@ export PATH=$PATH:/usr/local/go/bin
 export GOROOT=$HOME/go
 export PATH=$PATH:$GOROOT/bin:/home/user/github/swift/bin
 
-export PATH="`pwd`/mybin/:$PATH"
+export PATH="`pwd`/bin/:`pwd`/gohome/bin/:`pwd`/mybin/:$PATH"
 source ~/.local_bashrc
 
