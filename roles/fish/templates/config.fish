4a0c22309be470382ceb516dc05e28a526e8ff70
Kuo-tung Kao <ya790206@gmail.com>
2016-03-02 22:30:01 +0800

    Revert "use fisher instead of oh-my-fish"
    This reverts commit a87f0e93a96c7cbe4fdafca28b24ff191acdec55 [formerly 3e620e5666f6a0c64338d3ea77c1990e4fe7e8c7] [formerly 74e6804b5999248e749d068609d260f3a61f869c [formerly 57ee7888c84dbbe66e46aa048a6432e35c32b3bb]].
    
    
    Former-commit-id: d5223091f57d24d78eae63603048b97a82dc205d [formerly c6b43458b5910c6377f476fd2e8b214ccdcbae1e]
    Former-commit-id: 7232c4cbddb654bc6d748ffe3d22e8823e9f7cef

--- roles/fish/templates/config.fish
+++ roles/fish/templates/config.fish
@@ -1,14 +1,19 @@
-set fisher_home ~/.local/share/fisherman
-set fisher_config ~/.config/fisherman
-source $fisher_home/config.fish
+# Path to Oh My Fish install.
+set -gx OMF_PATH "{{ HOME_PATH }}/.local/share/omf"
 
-set -gx PATH "{{ HOME_PATH }}/gohome/bin"  "{{ HOME_PATH }}/bin"  "{{ HOME_PATH }}/mybin" $PATH
+# Customize Oh My Fish configuration path.
+set -gx OMF_CONFIG "/home/kjelly/.config/omf"
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
 
 alias cd1 "cd .."
 alias cd2 "cd ../.."
 alias cd3 "cd ../../.."
 alias cd4 "cd ../../../.."
 alias cd5 "cd ../../../../.."
+
+set -gx PATH "{{ HOME_PATH }}/gohome/bin"  "{{ HOME_PATH }}/bin"  "{{ HOME_PATH }}/mybin" $PATH
 
 eval (python -m virtualfish)
 
