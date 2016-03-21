9d164c1ca7d309484967ad5c1bfd798202b31b7d
Kuo-tung Kao <ya790206@gmail.com>
2016-03-21 23:39:36 +0800

    refactor open_term (i3 role)

--- roles/i3/files/i3-config/open_term.py
+++ roles/i3/files/i3-config/open_term.py
@@ -12,16 +12,17 @@ def shellquote(s):
 
 if __name__ == '__main__':
     try:
+        term = 'xfce4-terminal'
         wid, instance, title = [(i.get('window'), i.get('window_properties').get('instance'), i.get('window_properties').get('title')) for i in i3.filter(nodes=[]) if i.get('focused')][0]
         w = [i for i in i3.filter(nodes=[]) if i.get('focused')][0]
 
         if 'term' in instance and (' fish ' in title or ' zsh ' in title or ' bash ' in title):
-            cmd = 'xfce4-terminal --working-directory=(pwd)'
+            cmd = '%s --working-directory=(pwd)' % term
             os.system('''xdotool type --window %s  %s ''' % (wid, shellquote(cmd)))
             os.system('''xdotool key --window %s  Return ''' % wid)
         else:
-            os.system('''i3-msg "exec %s" ''' % 'xfce4-terminal')
+            os.system('''i3-msg "exec %s" ''' % term)
     except:
-        os.system('''i3-msg "exec %s" ''' % 'xfce4-terminal')
+        os.system('''i3-msg "exec %s" ''' % term)
 
 
