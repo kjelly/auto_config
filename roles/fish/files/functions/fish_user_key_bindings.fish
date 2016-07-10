e78bc5810bbaf99ecbb6b7f4cc545d7748e526c3
Kuo-tung Kao <ya790206@gmail.com>
2016-07-11 07:21:58 +0800

    update fish keybindings

--- roles/fish/files/functions/fish_user_key_bindings.fish
+++ roles/fish/files/functions/fish_user_key_bindings.fish
@@ -1,7 +1,8 @@
 function fish_user_key_bindings
     #fish_vi_key_bindings
     fish_default_key_bindings
-    bind -M insert \cl "forward-word"
     fzf_key_bindings
+    bind \af "forward-word"
+    bind \ab "backward-word"
 end
 
