53b9ee5dbebed958a5d3e0f36e9f86670038c022
Kuo-tung Kao <ya790206@gmail.com>
2016-07-21 23:30:34 +0800

    disable host color and cwd bg for speed(fish)
    Former-commit-id: 423d58a0b152f8ca9eee9a65c0ee5826ebff27f3 [formerly 84504d300dec7c61f885f4663e0467075b0a853d]
    Former-commit-id: 4956c9dd405dacafbb52b926a56b133827f93332

--- roles/fish/files/get_color.py
+++ roles/fish/files/get_color.py
@@ -8,6 +8,8 @@ def main():
     data = [ord(i) % 16 for i in data]
     if len(sys.argv) > 2:
         data = [i^15 for i in data]
+    if data[0] < 5 and data[1] < 5 and data[2] < 5:
+        data[0] += 4
     data = [hex(i)[2:].upper() for i in data]
     print(''.join(data))
 
