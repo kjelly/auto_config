b80ecbd62708b64df6fedd827d14de7fa30f13d4
Kuo-tung Kao <ya790206@gmail.com>
2016-07-23 01:00:10 +0800

    change the way to generate prompt bg(vim)
    Former-commit-id: 8d16c8a4e9c90d38bb86519afd5cbed11c251435

--- roles/fish/files/get_color.py
+++ roles/fish/files/get_color.py
@@ -7,7 +7,7 @@ def main():
     data = hashlib.sha256(sys.argv[1]).digest()[:3]
     data = [ord(i) % 16 for i in data]
     if len(sys.argv) > 2:
-        data = [i^15 for i in data]
+        data = [ 15 - i for i in data]
     if data[0] < 5 and data[1] < 5 and data[2] < 5:
         data[0] += 4
     data = [hex(i)[2:].upper() for i in data]
