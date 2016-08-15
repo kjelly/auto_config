9f54e4304a7e9dfff6722e768fbf2d2aa941910e
Kuo-tung Kao <ya790206@gmail.com>
2016-08-15 20:33:39 +0800

    add libssl to dependency
    Former-commit-id: ad3c8f19dcb37d0709c9a013eb3031c6dae04d08

--- install-deb-dependency.sh
+++ install-deb-dependency.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-sudo apt-get install -y python-dev git python-pip libffi-dev
+sudo apt-get install -y python-dev git python-pip libffi-dev libssl-dev
 sudo apt-get build-dep -y ansible python-cryptography
 sudo pip install ansible
 sudo pip install markupsafe
