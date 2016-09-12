4ea027faca5a6cb2167d31a78571b274683f1422
Kuo-tung Kao <ya790206@gmail.com>
2016-09-12 23:38:53 +0800

    set LC_ALL to C when install dependency
    Former-commit-id: 14a7209e45ecdcde29ae75177235d438af04448d [formerly face6802758fc302cd1416024bd5342e964a7f9c]
    Former-commit-id: 11061a227cb93456657ece6c600fef5b35de7172

--- install-deb-dependency.sh
+++ install-deb-dependency.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+export LC_ALL=C
 sudo apt-get install -y python-dev git python-pip libffi-dev libssl-dev
 sudo apt-get build-dep -y ansible python-cryptography
 sudo pip install ansible
