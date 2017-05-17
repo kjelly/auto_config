030dede304e06308b6db1438e8aa8c5a43aed557
Kuo-tung Kao <ya790206@gmail.com>
2017-05-17 12:56:39 -0400

    add upgrade action for atom

---
+++ library/atom_latest_version.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+from ansible.module_utils.basic import *
+import os
+from pyquery import PyQuery as pq
+
+def main():
+    module = AnsibleModule(argument_spec={})
+    d = pq(url='https://github.com/atom/atom/releases')
+    version = d(".label-latest .css-truncate-target").text()
+    module.exit_json(changed=False, VERSION=version)
+
+
+if __name__ == '__main__':
+    main()
+
