b75e63b25d6197f2e9ee585a8b7209fcb90ef79d
Kuo-tung Kao <ya790206@gmail.com>
2017-08-13 21:12:34 +0800

    add kotlin path(fish)

--- library/fish_path_generator.py
+++ library/fish_path_generator.py
@@ -34,7 +34,7 @@ def main():
                  '~/swif/usr/bin', '/usr/local/mercury-14.01.1/bin',
                  '/usr/lib/dart/bin/', '~/.cargo/bin/', '~/sbt/bin',
                  '~/.pub-cache/bin', '~/dart-sdk/bin', '~/activator/bin/',
-                 '~/google-cloud-sdk/bin/']
+                 '~/google-cloud-sdk/bin/', '~/kotlinc/bin/']
 
     for path in path_list:
         real_path = os.path.abspath(os.path.expanduser(path))
