0cc8c2eadb97c2885ae0af821dd8109b5c409799
Kuo-tung Kao <ya790206@gmail.com>
2018-06-01 00:30:17 +0800

    add new script to init nvim
    Former-commit-id: 192bc56dccd7705c087704424020a23fa22c47b5

---
+++ init_nvim.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo curl https://github.com/neovim/neovim/releases/download/v0.2.2/nvim.appimage -o /usr/bin/nvim
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+mkdir -p ~/.config/nvim/autoload/
+
+cp ~/.vim/autoload/plug.vim ~/.config/nvim/autoload/plug.vim
+
+curl https://myvimrc-205113.appspot.com/?programming=0 -o ~/.config/nvim/init.vim
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+
+~/.fzf/install
