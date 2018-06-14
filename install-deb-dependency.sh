236db0182f99a99af0624f0b5206ae386455ce46
Kuo-tung Kao <kjellytw@gmail.com>
2018-06-14 14:15:59 +0800

    udpate install-deb-dependency
    Former-commit-id: ab934d8db3a585ea66b85833f85d5665a897cae6

--- install-deb-dependency.sh
+++ install-deb-dependency.sh
@@ -1,10 +1,4 @@
 #! /bin/bash
 export LC_ALL=C
 sudo apt-get dist-upgrade -y
-sudo apt-get install -y python-dev git python-setuptools libffi-dev libssl-dev
-sudo apt-get build-dep -y ansible python-cryptography
-sudo easy_install pip
-sudo pip install --upgrade setuptools
-sudo pip install ansible
-sudo pip install markupsafe
-sudo pip install cryptography
+sudo apt-get install -y python-dev git python-setuptools libffi-dev libssl-dev ansible python-pip python3-pip
