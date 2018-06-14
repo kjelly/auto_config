8b233ffee34ffc7974a80d2794d2dcb3e4e4b561
Kuo-tung Kao <ya790206@gmail.com>
2018-06-15 01:59:20 +0800

    add new script to download nightly nvim
    Former-commit-id: 793ca5b5e47ce201d7b11939bb1c7bc273698685

---
+++ init_nvim_nightly.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#sudo curl -fL https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -o /opt/nvim.appimage.nightly
+sudo chmod +x /opt/nvim.appimage.nightly
+cd /opt/
+sudo rm -rf /opt/squashfs-root
+sudo /opt/nvim.appimage.nightly --appimage-extract
+sudo rm -rf /opt/neovim.nightly
+sudo mv squashfs-root neovim.nightly
+sudo find /opt/neovim.nightly/ -type d -exec chmod go+rx {} \;
+cd /usr/local/bin
+sudo ln -s /opt/neovim.nightly/AppRun nvim.nightly
+
