c0cf27aaf151d7be2ab208129506144fc4f3e600
Kuo-tung Kao <ya790206@gmail.com>
2018-06-15 01:32:47 +0800

    extract nvim from appimage(init_nvim)
    When you use nvim by appimage, the fzf window will not close if pressing
    esc.
    But the issue will be gone if you extract nvim from appimage.
    
    
    Former-commit-id: 42d517a7ea6960d5c6aa55e0ff13b727e3f80fcc

--- init_nvim.sh
+++ init_nvim.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-sudo curl -fL https://github.com/neovim/neovim/releases/download/v0.3.0/nvim.appimage -o /usr/bin/nvim
-
-sudo chmod +x /usr/bin/nvim
+sudo curl -fL https://github.com/neovim/neovim/releases/download/v0.3.0/nvim.appimage -o /opt/nvim.appimage
+sudo chmod +x /opt/nvim.appimage
+cd /opt/
+sudo /opt/nvim.appimage --appimage-extract
+sudo rm -rf /opt/neovim
+sudo mv squashfs-root neovim
+cd /usr/local/bin
+sudo find /opt/neovim/ -type d -exec chmod o+x {} \;
+sudo ln -s /opt/neovim/AppRun nvim
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
