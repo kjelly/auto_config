07bcffa50c25f9231e79705579e19f80aed9f215
Kuo-tung Kao <ya790206@gmail.com>
2018-06-15 02:00:52 +0800

    fix bug(init_nvim)
    Former-commit-id: 8e3e479f41efe470a4885b373db7b3052ba352de

--- init_nvim.sh
+++ init_nvim.sh
@@ -3,11 +3,12 @@
 sudo curl -fL https://github.com/neovim/neovim/releases/download/v0.3.0/nvim.appimage -o /opt/nvim.appimage
 sudo chmod +x /opt/nvim.appimage
 cd /opt/
+sudo rm -rf /opt/squashfs-root
 sudo /opt/nvim.appimage --appimage-extract
-sudo rm -rf /opt/neovim
 sudo mv squashfs-root neovim
+sudo rm -rf /opt/neovim
 cd /usr/local/bin
-sudo find /opt/neovim/ -type d -exec chmod o+x {} \;
+sudo find /opt/neovim/ -type d -exec chmod go+rx {} \;
 sudo ln -s /opt/neovim/AppRun nvim
 
 mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.`date +%F_%R`
