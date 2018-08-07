7de99b35ab36ade628085ba06624e339c00e8324
Kuo-tung Kao <ya790206@gmail.com>
2018-08-07 09:59:49 +0800

    Don't use appimage directly.
    FZF will break if using nvim appimage.
    
    
    Former-commit-id: 1221e63e49e8adc77befeec1fa8467c121c4ffdc

--- init_nvim.sh
+++ init_nvim.sh
@@ -2,20 +2,15 @@
 sudo curl -fL https://github.com/neovim/neovim/releases/download/v0.3.1/nvim.appimage -o /opt/nvim.appimage
 sudo chmod +x /opt/nvim.appimage
 
-if grep -q Microsoft /proc/version; then
-  #wsl
-  cd /opt/
-  sudo rm -rf /opt/squashfs-root
-  sudo /opt/nvim.appimage --appimage-extract
-  sudo rm -rf /opt/neovim
-  sudo mv squashfs-root neovim
-  cd /usr/local/bin
-  sudo find /opt/neovim/ -type d -exec chmod go+rx {} \;
-  sudo ln -s /opt/neovim/AppRun nvim
-else
-  cd /usr/local/bin
-  sudo ln -s /opt/nvim.appimage nvim
-fi
+cd /opt/
+sudo rm -rf /opt/squashfs-root
+sudo /opt/nvim.appimage --appimage-extract
+sudo rm -rf /opt/neovim
+sudo mv squashfs-root neovim
+cd /usr/local/bin
+sudo find /opt/neovim/ -type d -exec chmod go+rx {} \;
+sudo ln -s /opt/neovim/AppRun nvim
+
 mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.`date +%F_%R`
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
