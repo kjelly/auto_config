7dd53701ea71e33c6c33386e021f620901aa2fbc
Kuo-tung Kao <kjellytw@gmail.com>
2018-08-27 22:13:12 +0800

    Install pip using get-pip(install-deb-packages)
    Former-commit-id: 20ed0012581fca484dfde4141fe2ddc1cfa8e238

--- install-deb-dependency.sh
+++ install-deb-dependency.sh
@@ -1,4 +1,8 @@
-#! /bin/bash
+#! /binsymotion-prefix)/bash
 export LC_ALL=C
 sudo apt-get dist-upgrade -y
-sudo apt-get install -y python-dev git python-setuptools libffi-dev libssl-dev ansible python-pip python3-pip
+sudo apt-get install -y python-dev git python-setuptools libffi-dev libssl-dev python3-setuptools
+curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
+sudo python /tmp/get-pip.py
+sudo python3 /tmp/get-pip.py
+
