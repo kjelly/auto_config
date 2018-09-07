cf2167e23028472dbab5795eb66062fd132caeae
kjelly <@>
2018-09-08 00:00:31 +0800

    Update install-deb-dependency
    Former-commit-id: 956bef68a2f7f9dbc311985a488f957057384d6d

--- scripts/install-deb-dependency.sh
+++ scripts/install-deb-dependency.sh
@@ -3,6 +3,7 @@ export LC_ALL=C
 sudo apt-get dist-upgrade -y
 sudo apt-get install -y python-dev git python-setuptools libffi-dev libssl-dev python3-setuptools
 curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
-sudo python /tmp/get-pip.py
 sudo python3 /tmp/get-pip.py
+sudo python /tmp/get-pip.py
+sudo pip install ansible
 
