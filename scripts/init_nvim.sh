15d4b7dfa97efd26d15b75ce16c71c5307792df0
Kuo-tung Kao <kjellytw@gmail.com>
2019-02-01 14:13:28 +0000

    Use python3 by default
    Former-commit-id: 37562fda9367e3a36b0ff7c44c2b99ad4fa45694

--- scripts/init_nvim.sh
+++ scripts/init_nvim.sh
@@ -11,19 +11,3 @@ cd /usr/local/bin
 sudo find /opt/neovim/ -type d -exec chmod go+rx {} \;
 sudo ln -s /opt/neovim/AppRun nvim
 
-mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.`date +%F_%R`
-
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-mkdir -p ~/.config/nvim/autoload/
-
-cp ~/.vim/autoload/plug.vim ~/.config/nvim/autoload/plug.vim
-
-curl https://myvimrc.herokuapp.com/vimrc?programming=1 -o ~/.config/nvim/init.vim
-
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-
-~/.fzf/install --all
-
-touch  ~/.vim_custom.vim
