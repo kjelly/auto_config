808729f181cbbd0f2e803d09cbdb7873d853e585
Kuo-tung Kao <kjellytw@gmail.com>
2019-06-19 21:39:20 +0800

    Install ripgrep(scripts/init_nvimrc)
    Former-commit-id: 8cfa4bb1f410df6143abc2b2a5848072cb5d5ba6

--- scripts/init_nvimrc.sh
+++ scripts/init_nvimrc.sh
@@ -1,3 +1,4 @@
+export RIPGREP_VERSION=11.0.1
 mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.`date +%F_%R`
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
@@ -8,6 +9,14 @@ mkdir -p ~/.config/nvim/autoload/
 cp ~/.vim/autoload/plug.vim ~/.config/nvim/autoload/plug.vim
 
 curl https://myvimrc.herokuapp.com/vimrc?programming=1 -o ~/.config/nvim/init.vim
+
+mkdir -p ~/.config/nvim/bin
+
+curl -L https://github.com/BurntSushi/ripgrep/releases/download/$RIPGREP_VERSION/ripgrep-$RIPGREP_VERSION-x86_64-unknown-linux-musl.tar.gz -o /tmp/a.tar.gz
+
+tar zxvf /tmp/a.tar.gz -C /tmp
+
+cp /tmp/ripgrep*/rg ~/.config/nvim/bin
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
