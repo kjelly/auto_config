65d79d5000cb3bebacf5b44f82eb43840ecaddbd
Kuo-tung Kao <kjellytw@gmail.com>
2019-06-19 23:28:16 +0800

    Update
    Former-commit-id: 65b045a76ede5153735c7027d6d0fbc030c520f6

--- scripts/install-deb-dependency.sh
+++ scripts/install-deb-dependency.sh
@@ -2,7 +2,7 @@
 export LC_ALL=C
 sudo apt update
 sudo apt-get dist-upgrade -y
-sudo apt-get install -y python-dev git python-setuptools libffi-dev libssl-dev python3-setuptools
+sudo apt-get install -y python-dev git python-setuptools libffi-dev libssl-dev python3-setuptools curl
 curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
 sudo python3 /tmp/get-pip.py
 sudo python /tmp/get-pip.py
