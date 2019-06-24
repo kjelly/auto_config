c3f8bd85de18223b8f961dbb81a4b479c6d5ee24
Kuo-tung Kao <kjellytw@gmail.com>
2019-06-24 19:29:06 +0800

    Add alt-i for inserting "herm-notify"(fish)
    Former-commit-id: 88e9699c450f29de150dd962840bc2b10f9acc77

--- roles/fish/files/fish/functions/fish_user_key_bindings.fish
+++ roles/fish/files/fish/functions/fish_user_key_bindings.fish
@@ -1,8 +1,13 @@
+function fish_notify
+       commandline -i -- ";hterm-notify.sh done "
+end
+
 function fish_user_key_bindings
     #fish_vi_key_bindings
     fish_default_key_bindings
     fzf_key_bindings
     bind \af "forward-word"
     bind \ab "backward-word"
+    bind \ei fish_notify
 end
 
