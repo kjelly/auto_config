e96e8af292c37232594e908b5dbf7a261545961e
Kuo-tung Kao <kjellytw@gmail.com>
2019-07-22 19:07:36 +0800

    Enable vi keybinding for fish
    I use 60% keyboard which is without the arrow keys.
    I need another way for the arrow keys.
    Use vi mode for easy.
    
    
    Former-commit-id: 789b767cfa6d9449636310bdc3054479fdbf5014

--- roles/fish/files/fish/functions/fish_user_key_bindings.fish
+++ roles/fish/files/fish/functions/fish_user_key_bindings.fish
@@ -3,11 +3,19 @@ function fish_notify
 end
 
 function fish_user_key_bindings
-    #fish_vi_key_bindings
-    fish_default_key_bindings
+    fish_vi_key_bindings
     fzf_key_bindings
-    bind \af "forward-word"
-    bind \ab "backward-word"
-    bind \ei fish_notify
+    bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
+    bind -M insert \ef "forward-word"
+    bind -M insert \eb "backward-word"
+
+    bind -M insert \cp up-or-search
+    bind -M insert \cn down-or-search
+
+    bind -M insert \cf end-of-line
+    bind -M insert \ce end-of-line
+    bind -M insert \ca beginning-of-line
+
+    bind -M insert \ei fish_notify
 end
 
