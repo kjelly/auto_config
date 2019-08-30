115ee3e07b9dc5681cb05dc9c83005f3bbb6a7cf
Kuo-tung Kao <kjellytw@gmail.com>
2019-08-30 14:12:55 +0800

    Fix bug
    Former-commit-id: 422ce46988ceae15f1732f828009975d3e38c508

--- scripts/init_nvimrc.sh
+++ scripts/init_nvimrc.sh
@@ -5,6 +5,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 mkdir -p ~/.config/nvim/autoload/
+mkdir -p ~/.config/nvim/bin/
 
 cp ~/.vim/autoload/plug.vim ~/.config/nvim/autoload/plug.vim
 
