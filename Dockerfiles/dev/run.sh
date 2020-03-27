ab208392c9270f7273a0261e1ba07238edd48c25
Kuo-tung Kao <kjellytw@gmail.com>
2020-03-27 21:21:32 +0800

    Update dev container
    Former-commit-id: 65ce8464eb0bcf4307b69c504af8c0ae2dade8b4

--- Dockerfiles/dev/run.sh
+++ Dockerfiles/dev/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --name dev -v $HOME/nfs:/root/nfs dev:latest
+docker run -d --name dev -v home:/home/ubuntu  -p 6080:80 -p 7000-7100:7000-7100 -v /dev/shm:/dev/shm/ -v /tmp/:/tmp/ -e USER=ubuntu -e PASSWORD=password  dev:latest
