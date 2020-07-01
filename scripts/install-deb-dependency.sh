57dc36d50f85e572d960fa8c3abbc3a334480833
Kuo-tung Kao <kjellytw@gmail.com>
2020-07-02 00:04:26 +0800

    Don't install pip2
    Former-commit-id: de4c5c5ba37ba880c7bdd7ad4df14999d7bd02c7

--- scripts/install-deb-dependency.sh
+++ scripts/install-deb-dependency.sh
@@ -5,6 +5,5 @@ sudo apt-get dist-upgrade -y
 sudo apt-get install -y python-dev git python-setuptools libffi-dev libssl-dev python3-setuptools curl
 curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
 sudo python3 /tmp/get-pip.py
-sudo python /tmp/get-pip.py
 sudo pip install ansible
 
