60c257a22b2d7f372507181f806375fb9d328e50
Kuo-tung Kao <kjellytw@gmail.com>
2021-02-16 17:34:11 +0800

    Update keybindings(vim)
    Former-commit-id: 814cbd732bb525371ef9124311b92af902a9f5e8

---
+++ roles/vim/templates/config.lua
@@ -0,0 +1,42 @@
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  highlight = {
+    enable = true,              -- false will disable the whole extension
+    disable = { },  -- list of language that will be disabled
+  },
+}
+
+local dap = require('dap')
+
+dap.adapters.python = {
+  type = 'executable';
+  command = '/usr/bin/python3';
+  args = { '-m', 'debugpy.adapter' };
+}
+
+dap.configurations.python = {
+  {
+    -- The first three options are required by nvim-dap
+    type = 'python'; -- the type here established the link to the adapter definition: `dap.adapters.python`
+    request = 'launch';
+    name = "Launch file";
+
+    -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+    program = "${file}"; -- This configuration will launch the current file if used.
+    pythonPath = function()
+      -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+      -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+      -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+      local cwd = vim.fn.getcwd()
+      if vim.fn.executable(cwd .. '/venv/bin/python') then
+        return '/usr/bin/python3'
+      elseif vim.fn.executable(cwd .. '/.venv/bin/python') then
+        return '/usr/bin/python3'
+      else
+        return '/usr/bin/python3'
+      end
+    end;
+  },
+}
+
