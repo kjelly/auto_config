37d02f35dde16ac18103300be77a63aae59defcf
Kuo-tung Kao <kjellytw@gmail.com>
2021-04-16 20:09:13 +0800

    Change the status line(vim)
    Former-commit-id: b3abd91bdc1fa96586db88d5a8717e434efdbbc2

--- roles/vim/templates/config.lua
+++ roles/vim/templates/config.lua
@@ -59,10 +59,15 @@ end
 
 if IsModuleAvailable("lualine") then
     require('lualine').setup{
+        options = {
+          theme = 'gruvbox_material',
+          section_separators = {'', ''},
+          component_separators = {'', ''},
+        },
         sections = {
           lualine_a = {'mode'},
-          lualine_b = {'branch'},
-          lualine_c = {'filename'},
+          lualine_b = {'branch', 'diff'},
+          lualine_c = {'hostname', 'filename'},
           lualine_x = {'encoding', 'fileformat', 'filetype'},
           lualine_y = {'progress'},
           lualine_z = {'location'}
