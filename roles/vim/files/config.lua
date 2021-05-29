c7b9d1bc09296f7884d05e6d7f19e5a467a2a220
Kuo-tung Kao <kjellytw@gmail.com>
2021-05-29 20:02:03 +0800

    Enable rainbow(vim)
    Former-commit-id: c42921f5a43006a206f619ae15ab8b17177523f5

--- roles/vim/files/config.lua
+++ roles/vim/files/config.lua
@@ -24,6 +24,11 @@ if IsModuleAvailable("nvim-treesitter") then
         highlight = {
             enable = true, -- false will disable the whole extension
             disable = { } -- list of language that will be disabled
+        },
+        rainbow = {
+          enable = true,
+          extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
+          max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
         }
     }
 end
