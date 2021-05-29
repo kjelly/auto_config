692fe8925001b2e158d2f740cf37d9769fec5ab8
Kuo-tung Kao <kjellytw@gmail.com>
2021-05-29 13:03:44 +0800

    Add new script
    Former-commit-id: 2cb4da24c23db25972d1e27a59da98f803277e82

---
+++ scripts/init_nvim_nightly_appimage.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo curl -fL https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -o /opt/nvim.appimage.nightly
+sudo chmod +x /opt/nvim.appimage.nightly
+cd /opt/
+sudo cp /opt/nvim.appimage.nightly /usr/bin/nvim
