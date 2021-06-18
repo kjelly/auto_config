c00a98022cdaa1a48cf7ae048b6cf084904aa6b5
Kuo-tung Kao <kjellytw@gmail.com>
2021-06-18 22:33:11 +0800

    Add textobj and fold(vim)
    Former-commit-id: 90deb309dde8fdf85366c5f6e1f493d7e1427e14

--- roles/vim/files/config.lua
+++ roles/vim/files/config.lua
@@ -31,6 +31,65 @@ if IsModuleAvailable("nvim-treesitter") then
           max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
         }
     }
+    require'nvim-treesitter.configs'.setup {
+      textobjects = {
+        select = {
+          enable = true,
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+
+            -- Or you can define your own textobjects like this
+            ["iF"] = {
+              python = "(function_definition) @function",
+              cpp = "(function_definition) @function",
+              c = "(function_definition) @function",
+              java = "(method_declaration) @function",
+            },
+          },
+        },
+      },
+    }
+    require'nvim-treesitter.configs'.setup {
+      textobjects = {
+        swap = {
+          enable = true,
+          swap_next = {
+            ["<leader>lsa"] = "@parameter.inner",
+          },
+          swap_previous = {
+            ["<leader>lsA"] = "@parameter.inner",
+          },
+        },
+      },
+    }
+    require'nvim-treesitter.configs'.setup {
+      textobjects = {
+        move = {
+          enable = true,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = {
+            ["]m"] = "@function.outer",
+            ["]]"] = "@class.outer",
+          },
+          goto_next_end = {
+            ["]M"] = "@function.outer",
+            ["]["] = "@class.outer",
+          },
+          goto_previous_start = {
+            ["[m"] = "@function.outer",
+            ["[["] = "@class.outer",
+          },
+          goto_previous_end = {
+            ["[M"] = "@function.outer",
+            ["[]"] = "@class.outer",
+          },
+        },
+      },
+    }
 end
 
 if IsModuleAvailable("dap") then
