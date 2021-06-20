f476fae6f3b3a6647b357b87fd96f1c96d9286d0
Kuo-tung Kao <kjellytw@gmail.com>
2021-06-20 16:55:52 +0800

    Add more treesitter plugin(vim)
    Former-commit-id: 84f8eed982264ef9219d8866daff1a4cf3e12a63

--- roles/vim/files/config.lua
+++ roles/vim/files/config.lua
@@ -21,6 +21,18 @@ end
 if IsModuleAvailable("nvim-treesitter") then
     require "nvim-treesitter.configs".setup {
         ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+        autopairs = {
+          enable = true,
+        },
+        iswap = {
+          enable = true,
+        },
+        incremental_selection = {
+          enable = true,
+        },
+        indent = {
+          enable = true,
+        },
         highlight = {
             enable = true, -- false will disable the whole extension
             disable = { } -- list of language that will be disabled
@@ -97,6 +109,32 @@ if IsModuleAvailable("nvim-treesitter") then
         },
       },
     }
+
+    require'treesitter-context.config'.setup{
+      enable = true,
+    }
+
+    require "nvim-treesitter.configs".setup {
+      playground = {
+        enable = true,
+        disable = {},
+        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+        persist_queries = false, -- Whether the query persists across vim sessions
+        keybindings = {
+          toggle_query_editor = 'o',
+          toggle_hl_groups = 'i',
+          toggle_injected_languages = 't',
+          toggle_anonymous_nodes = 'a',
+          toggle_language_display = 'I',
+          focus_language = 'f',
+          unfocus_language = 'F',
+          update = 'R',
+          goto_node = '<cr>',
+          show_help = '?',
+        },
+      }
+    }
+
 end
 
 if IsModuleAvailable("dap") then
