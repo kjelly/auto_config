78347adf6422678d30b66b109745789f36933b90
Kuo-tung Kao <kjellytw@gmail.com>
2021-07-01 19:42:11 +0800

    Add more scripts(mybin)
    Former-commit-id: 03bcdcc25afe6af0f5725133f7ad226a46bf02e5

---
+++ roles/mybin/files/mybin/random-terminal-font.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+font=`fc-list :mono|grep Nerd|choose -f ':' 1|shuf|head -n 1|sed -e 's/^[[:space:]]*//'`
+size=16
+
+cp ~/.alacritty.yml ~/a.yaml
+yq e ".font.normal.family=\"$font\"" ~/a.yaml -i
+yq e ".font.bold.family=\"$font\"" ~/a.yaml -i
+yq e ".font.italic.family=\"$font\"" ~/a.yaml -i
+yq e ".font.bold_italic.family=\"$font\"" ~/a.yaml -i
+yq e ".font.size=$size" ~/a.yaml -i
+
+cp ~/a.yaml ~/.alacritty.yml
+
+
+crudini --set ~/.config/xfce4/terminal/terminalrc 'Configuration' 'FontName' "$font $size"
---
+++ roles/mybin/files/mybin/test-true-color.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+#
+#   This file echoes a bunch of 24-bit color codes
+#   to the terminal to demonstrate its functionality.
+#   The foreground escape sequence is ^[38;2;<r>;<g>;<b>m
+#   The background escape sequence is ^[48;2;<r>;<g>;<b>m
+#   <r> <g> <b> range from 0 to 255 inclusive.
+#   The escape sequence ^[0m returns output to default
+
+setBackgroundColor()
+{
+    echo -en "\x1b[48;2;$1;$2;$3""m"
+}
+
+resetOutput()
+{
+    echo -en "\x1b[0m\n"
+}
+
+# Gives a color $1/255 % along HSV
+# Who knows what happens when $1 is outside 0-255
+# Echoes "$red $green $blue" where
+# $red $green and $blue are integers
+# ranging between 0 and 255 inclusive
+rainbowColor()
+{
+    let h=$1/43
+    let f=$1-43*$h
+    let t=$f*255/43
+    let q=255-t
+
+    if [ $h -eq 0 ]
+    then
+        echo "255 $t 0"
+    elif [ $h -eq 1 ]
+    then
+        echo "$q 255 0"
+    elif [ $h -eq 2 ]
+    then
+        echo "0 255 $t"
+    elif [ $h -eq 3 ]
+    then
+        echo "0 $q 255"
+    elif [ $h -eq 4 ]
+    then
+        echo "$t 0 255"
+    elif [ $h -eq 5 ]
+    then
+        echo "255 0 $q"
+    else
+        # execution should never reach here
+        echo "0 0 0"
+    fi
+}
+
+for i in `seq 0 127`; do
+    setBackgroundColor $i 0 0
+    echo -en " "
+done
+resetOutput
+for i in `seq 255 128`; do
+    setBackgroundColor $i 0 0
+    echo -en " "
+done
+resetOutput
+
+for i in `seq 0 127`; do
+    setBackgroundColor 0 $i 0
+    echo -n " "
+done
+resetOutput
+for i in `seq 255 128`; do
+    setBackgroundColor 0 $i 0
+    echo -n " "
+done
+resetOutput
+
+for i in `seq 0 127`; do
+    setBackgroundColor 0 0 $i
+    echo -n " "
+done
+resetOutput
+for i in `seq 255 128`; do
+    setBackgroundColor 0 0 $i
+    echo -n " "
+done
+resetOutput
+
+for i in `seq 0 127`; do
+    setBackgroundColor `rainbowColor $i`
+    echo -n " "
+done
+resetOutput
+for i in `seq 255 128`; do
+    setBackgroundColor `rainbowColor $i`
+    echo -n " "
+done
+resetOutput
+
+#!/bin/bash
+# Based on: https://gist.github.com/XVilka/8346728
+
+awk -v term_cols="${width:-$(tput cols || echo 80)}" 'BEGIN{
+    s="/\\";
+    for (colnum = 0; colnum<term_cols; colnum++) {
+        r = 255-(colnum*255/term_cols);
+        g = (colnum*510/term_cols);
+        b = (colnum*255/term_cols);
+        if (g>255) g = 510-g;
+        printf "\033[48;2;%d;%d;%dm", r,g,b;
+        printf "\033[38;2;%d;%d;%dm", 255-r,255-g,255-b;
+        printf "%s\033[0m", substr(s,colnum%2+1,1);
+    }
+    printf "\n";
+}'
+
+
