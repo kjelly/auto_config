05d73f995a091359c3695ddcc37c1298f54d88af
Kuo-tung Kao <kjellytw@gmail.com>
2021-09-12 11:20:08 +0800

    Add new script, init_zsh
    Former-commit-id: f85e500927e12ef80665e386902fbfe8529f83e1

---
+++ scripts/init_zsh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
