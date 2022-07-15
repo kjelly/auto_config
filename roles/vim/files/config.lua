d053dd791d4ec1514b3aa88e2cb27b355f26c52c
Kuo-tung Kao <kjellytw@gmail.com>
2022-07-15 14:03:59 +0000

    Add nvim support for lua lang server
    Former-commit-id: f402f818133046108edb6f01e15cc6ed45702c9c

--- roles/vim/files/config.lua
+++ roles/vim/files/config.lua
@@ -704,6 +704,28 @@ if IsModuleAvailable("lspconfig") then
     require('lspconfig')[lsp].setup {
     }
   end
+  require'lspconfig'.sumneko_lua.setup {
+    settings = {
+      Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT',
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = {'vim'},
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = vim.api.nvim_get_runtime_file("", true),
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+          enable = false,
+        },
+      },
+    },
+  }
 end
 if IsModuleAvailable("marks") then
   require'marks'.setup {
