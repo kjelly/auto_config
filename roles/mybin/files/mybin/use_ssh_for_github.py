f1fbe1daa23d5f04b6998e515497e13e739091da
Kuo-tung Kao <kjellytw@gmail.com>
2022-08-06 13:12:10 +0000

    Update use_ssh_for_github(mybin)
    Former-commit-id: 2bd270b62b3b38ec6e366e2d93f2f98ddf16423a

--- roles/mybin/files/mybin/use_ssh_for_github.py
+++ roles/mybin/files/mybin/use_ssh_for_github.py
@@ -3,21 +3,22 @@ from subprocess import check_output
 import os
 import re
 
+
 def get_remote_url():
     output = check_output("git remote -v|grep push", shell=True).decode('utf-8')
     return output
 
+
 def get_username_and_repository_from_http_url(url):
-    result = re.search('https://github.com/(?P<username>\w+)/(?P<respository>(\w|[\-_])+)(\.git)?', url)
+    result = re.search(r'https://github.com/(?P<username>\w+)/(?P<respository>(\w|[\-_])+)(\.git)?', url)
     if result:
         return result.groupdict()
     ret = {}
-    ret['username'] = raw_input("Please input your user name.")
-    ret['respository'] = raw_input("Please input your respository name.")
+    ret['username'] = input("Please input your user name.")
+    ret['respository'] = input("Please input your respository name.")
     return ret
 
 
 url = get_remote_url()
 git_info = get_username_and_repository_from_http_url(url)
 os.system("git remote set-url --push origin git@github.com:%s/%s.git" % (git_info['username'], git_info['respository']))
-
