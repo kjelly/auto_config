ae297042804a60eb0bf8c9c38e5425746c19d6d0
Kuo-tung Kao <kjellytw@gmail.com>
2022-08-07 11:17:12 +0000

    Update random-color-scheme.py
    Former-commit-id: 239520cdf4051b19343db4f13e96b3cdd233543b

--- roles/vim/files/random-color-scheme.py
+++ roles/vim/files/random-color-scheme.py
@@ -1,58 +1,46 @@
 #!/usr/bin/env python3
+import glob
+import json
 import os
 import random
+import sys
 
-colors = [
-    'aquarium',
-    'material',
-    'aurora',
-    'moonlight',
-    'everforest',
-    'calvera',
-    'github-colors',
-    'gruvbox',
-    'gruvbox-flat',
-    'nightfox',
-    'darcula-solid',
-    'dracula',
-    'gruvbox-material',
-    'tokyonight',
-    'onedark',
-    'darkplus',
-    'edge',
-]
 
-bad_in_dark_mode = [
-    "onedark",
-    "dracula",
-    "deep-space",
-    "moonfly",
-    "ayu",
-    "iceberg",
-    "PaperColor",
-    "doom-one",
-    "darcula-solid",
-]
+def find_all_color_schemes():
+    nvim_path = os.path.expanduser('~/.config/nvim/plugged/')
+    color_scheme_dir = []
+    for i in os.listdir(nvim_path):
+        p = os.path.abspath(os.path.join(nvim_path, i, 'colors'))
+        if os.path.exists(p):
+            color_scheme_dir.append(p)
 
-treesitter = [
-    "darcula-solid",  # treesitter
-    "dracula",  # treesitter
-    "material",  # treesitter
-    "gruvbox-flat",  # treesitter
-    "tokyonight",  # treesitter
-    "darkplus",  # treesitter
-    "aurora",
-    'aquarium',
-    'edge',
-    'everforest',
-]
+    ret = []
+    for i in color_scheme_dir:
+        ret.extend(glob.glob(f'{i}/*.vim'))
+    ret = [os.path.basename(i)[:-4] for i in ret]
+    return ret
 
-color = random.choice(list(dict.fromkeys(treesitter)))
-with open(os.path.expanduser('~/.vim_custom.vim'), 'r') as ftr:
-    data = ftr.read().strip().split('\n')
 
-data = [i for i in data if 'colorscheme' not in i]
-data.append(f'colorscheme {color}')
+def main():
+    colors = []
+    color_json_path = os.path.expanduser('~/.colors.json')
+    if len(sys.argv) > 1 or not os.path.isfile(color_json_path):
+        colors = find_all_color_schemes()
+        with open(color_json_path, 'w') as f:
+            json.dump(colors, f)
+    else:
+        with open(color_json_path, 'r') as f:
+            colors = json.load(f)
 
-with open(os.path.expanduser('~/.vim_custom.vim'), 'w') as ftr:
-    ftr.write('\n'.join(data))
+    color = random.choice(colors)
+    with open(os.path.expanduser('~/.vim_custom.vim'), 'r') as ftr:
+        data = ftr.read().strip().split('\n')
+    data = [i for i in data if 'colorscheme' not in i]
+    data.append(f'colorscheme {color}')
+
+    with open(os.path.expanduser('~/.vim_custom.vim'), 'w') as ftr:
+        ftr.write('\n'.join(data))
+
+
+if __name__ == "__main__":
+    main()
