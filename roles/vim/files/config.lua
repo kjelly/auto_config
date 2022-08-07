921eb50aebea167ff0293f20889cb087eeb961e7
Kuo-tung Kao <kjellytw@gmail.com>
2022-08-07 08:05:37 +0000

    Use nvim_lsp for bufferline.nvim
    Former-commit-id: 6ea91ed5a5c63747ab757d3d5e6f4787c0fc48e6

--- roles/vim/files/config.lua
+++ roles/vim/files/config.lua
@@ -379,7 +379,7 @@ end
 if IsModuleAvailable("bufferline") then
   require 'bufferline'.setup {
     options = {
-      diagnostics = "coc",
+      diagnostics = "nvim_lsp",
       diagnostics_indicator = function(count, level, diagnostics_dict, context)
         local s = " "
         for e, n in pairs(diagnostics_dict) do
