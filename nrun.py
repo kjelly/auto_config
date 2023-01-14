a02c171aa9d46bc55c416a96264c98dcbacf018e
Kuo-tung Kao <kjellytw@gmail.com>
2023-01-14 16:09:55 +0000

    Format
    Former-commit-id: 2f9f1f2364f99f558140592e03e6523ceba64512

--- nrun.py
+++ nrun.py
@@ -25,39 +25,45 @@ def find_base_dir():
 
 def main():
     base_dir = find_base_dir()
-    inventory_path = os.path.join(base_dir, 'hosts')
-    parser = argparse.ArgumentParser(description='deploy script')
-    parser.add_argument('-i', '--inventory', type=str, default=inventory_path)
-    parser.add_argument('--host', type=str, default='local')
-    parser.add_argument('-a', '--action', type=str,
-                        default='config')
-    parser.add_argument('-u', '--user', type=str, default=getpass.getuser())
-    parser.add_argument('--sudo', action='store_true', default=False)
-    parser.add_argument('--role', '-r', nargs='+', required=True)
+    inventory_path = os.path.join(base_dir, "hosts")
+    parser = argparse.ArgumentParser(description="deploy script")
+    parser.add_argument("-i", "--inventory", type=str, default=inventory_path)
+    parser.add_argument("--host", type=str, default="local")
+    parser.add_argument("-a", "--action", type=str, default="config")
+    parser.add_argument("-u", "--user", type=str, default=getpass.getuser())
+    parser.add_argument("--sudo", action="store_true", default=False)
+    parser.add_argument("--role", "-r", nargs="+", required=True)
 
     args, unknown = parser.parse_known_args()
     print(args)
     print(unknown)
 
     data = {
-        'user': args.user,
-        'action': args.action,
-        'inventory': args.inventory,
-        'roles': args.role,
-        'programming': True
+        "user": args.user,
+        "action": args.action,
+        "inventory": args.inventory,
+        "roles": args.role,
+        "programming": True,
     }
-    sudo = 'sudo -E -P -u {user} ' if args.sudo else ''
+    sudo = "sudo -E -P -u {user} " if args.sudo else ""
 
-    if args.host == 'local':
-        data['group'] = 'local'
-        data['remote_host'] = None
+    if args.host == "local":
+        data["group"] = "local"
+        data["remote_host"] = None
     else:
-        data['group'] = 'remote'
-        data['remote_host'] = args.host
+        data["group"] = "remote"
+        data["remote_host"] = args.host
 
     stdin_data = {}
     with suppress(Exception):
-        if select.select([sys.stdin, ], [], [], 0.0)[0]:
+        if select.select(
+            [
+                sys.stdin,
+            ],
+            [],
+            [],
+            0.0,
+        )[0]:
             stdin_data = json.loads(sys.stdin.read())
 
     data.update(stdin_data)
@@ -65,21 +71,27 @@ def main():
     template = get_template()
     outputText = template.render(**data)
 
-    f = tempfile.NamedTemporaryFile(delete=False, prefix='auto_config')
-    f.write(outputText.encode('utf-8'))
+    f = tempfile.NamedTemporaryFile(delete=False, prefix="auto_config")
+    f.write(outputText.encode("utf-8"))
     f.flush()
 
-    data['playbook'] = f.name
+    data["playbook"] = f.name
 
-    cmd = ((sudo + '''ansible-playbook -i "{inventory}" '''
-            '''"{playbook}" -e ansible_user="{user}" -e group="{group}" '''
-            '''-e remote_host="{remote_host}" -e action="{action}" '''
-            ''' ''').format(**data) + ' '.join(unknown))
+    cmd = (
+        sudo + """ansible-playbook -i "{inventory}" """
+        """"{playbook}" -e ansible_user="{user}" -e group="{group}" """
+        """-e remote_host="{remote_host}" -e action="{action}" """
+        """ """
+    ).format(**data) + " ".join(unknown)
     print(cmd)
+
     os.system(cmd)
+
     f.close()
+    return
     os.remove(f.name)
+    a = xyz
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
