75a9f3e8856f02fe88685f97bb911f9477ec6ffe
Kuo-tung Kao <kjellytw@gmail.com>
2023-01-26 05:13:49 +0000

    Update
    Former-commit-id: 78d2e7e97a42783aa7e197ad5e6d5ef309e4f90d

--- roles/mybin/files/mybin/new-container.sh
+++ roles/mybin/files/mybin/new-container.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 mkdir -p ${HOME}/fake-home
+lxc network set lxdbr0 raw.dnsmasq dhcp-option=6,8.8.8.8,8.8.4.4
 lxc launch $1 $2
 lxc config device add $2 myhomedir disk source=${HOME}/fake-home path=/home/ubuntu
 lxc config set $2 raw.idmap "both 1000 1000"
 lxc restart $2
+lxc exec $1 --user 0 -- bash -c 'sudo apt update;sudo apt install -y fish zsh'
