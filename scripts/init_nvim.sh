fe4817dca6277f59c3c77d5ead83c26a0f33f18b
Kuo-tung Kao <kjellytw@gmail.com>
2023-04-10 02:19:03 +0000

    Use new way to download nvim

--- scripts/init_nvim.sh
+++ scripts/init_nvim.sh
@@ -5,8 +5,7 @@ version=$(git -c 'versionsort.suffix=-' \
     | tail --lines=1 \
     | cut --delimiter='/' --fields=3)
 echo $version
-#sudo curl -fL https://github.com/neovim/neovim/releases/download/$version/nvim.appimage -o /opt/nvim.appimage
-#sudo chmod +x /opt/nvim.appimage
-#sudo cp /opt/nvim.appimage /bin/nvim
-wget https://github.com/neovim/neovim/releases/download/$version/nvim-linux64.deb -O nvim.deb
-sudo dpkg -i nvim.deb
+
+
+sudo curl -L https://github.com/neovim/neovim/releases/download/$version/nvim.appimage --output /usr/local/bin/nvim
+sudo chmod +x /usr/local/bin/nvim
