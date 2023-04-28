13009c40a97891a5aa9eab066110ae61e3068ca7
Kuo-tung Kao <kjellytw@gmail.com>
2023-04-28 13:53:28 +0000

    Improve p.py(pueue)

--- roles/pueue/files/p.py
+++ roles/pueue/files/p.py
@@ -2,13 +2,33 @@
 import time
 import argparse
 import os
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, check_output
 import json
 import readline
 import atexit
 
 histfile = os.path.join(os.path.expanduser("~"), ".python_history")
 script_path = os.path.realpath(__file__)
+
+
+def pueue_status():
+    return json.loads(check_output([
+        "pueue", "status", "-j"]).decode("utf-8"))
+
+
+def get_latest_task_id():
+    latest_date = ''
+    latest_id = ''
+    dict = pueue_status()
+    tasks = dict["tasks"]
+    for i in tasks:
+        obj = tasks[i]
+        if obj["start"] is None:
+            continue
+        if obj["start"] > latest_date:
+            latest_date = obj["start"]
+            latest_id = i
+    return latest_id
 
 
 def fzf(items):
@@ -34,19 +54,6 @@ def shell(history):
             print('')
 
 
-def shell1():
-    rl = readline.get_instance()
-    rl.set_prompt('>>> ')
-    while True:
-        try:
-            line = rl.readline()
-            if line == 'stop':
-                break
-            os.system(f"{script_path} {line}")
-        except KeyboardInterrupt:
-            print('')
-
-
 def main():
     parser = argparse.ArgumentParser()
     args, rest = parser.parse_known_args()
@@ -60,15 +67,33 @@ def main():
         try:
             with open(config_path, "r") as f:
                 config = json.load(f)
-        except:
+        except Exception:
             pass
     if "history" not in config:
         config["history"] = []
 
     command = rest[0]
-    if command in ["follow", "add", "clean",
+    if command in ["follow", "add", "clean", "remove",
                    "restart", "kill", "pause", "log", "wait"]:
         os.system(f"pueue {command} {' '.join(rest[1:])}")
+    elif command in ["restart", "follow", "remove", "kill", "pause", "start"]:
+        if len(rest) == 1:
+            latest_id = get_latest_task_id()
+            os.system(f"pueue {command} {latest_id}")
+        else:
+            os.system(f"pueue {command} {' '.join(rest[1:])}")
+    elif command == "fl":
+        latest_id = get_latest_task_id()
+        os.system(f"pueue follow {latest_id}")
+    elif command == "f":
+        if len(rest) == 1:
+            os.system("pueue follow")
+        elif rest[1] in ["-1", ":"]:
+            latest_id = get_latest_task_id()
+            os.system(f"pueue follow {latest_id}")
+        else:
+            os.system(f"pueue follow {' '.join(rest[1:])}")
+
     elif command == "fzf":
         history = [' '.join(i) for i in config["history"]]
         print(fzf(history))
@@ -80,8 +105,9 @@ def main():
         p = Popen(["pueue", "add", *rest], stdout=PIPE, stderr=PIPE)
         p.wait()
         task_id = p.stdout.readline().decode("utf-8")[19:-3]
-        time.sleep(1)
-        os.system(f"pueue follow {task_id}")
+        tasks = pueue_status()["tasks"]
+        if task_id in tasks and tasks[task_id]["start"]:
+            os.system(f"pueue follow {task_id}")
 
     with open(config_path, "w") as f:
         json.dump(config, f)
