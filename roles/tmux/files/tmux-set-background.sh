68fdbe0510c96bb92904fd95d61e545c94096cc4
Kuo-tung Kao <kjellytw@gmail.com>
2023-07-23 07:46:41 +0000

    Update

--- roles/tmux/files/tmux-set-background.sh
+++ roles/tmux/files/tmux-set-background.sh
@@ -2,7 +2,7 @@
 
 width=$(tmux display -p '#{client_width}')
 
-if [ $width -gt 200 ]; then
+if [ $width -gt 130 ] && [ $width -le 140 ]; then
     tmux set -g window-style 'fg=#171421,bg=#ffffff'
 else
     tmux set -g window-style 'fg=#d0cfcc,bg=#171421'
