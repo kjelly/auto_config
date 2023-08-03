06c3d696b9ce2406efa76d3e1a353eac8156eb2f
Kuo-tung Kao <kjellytw@gmail.com>
2023-08-03 10:50:28 +0800

    Update

--- scripts/init_pueue.sh
+++ scripts/init_pueue.sh
@@ -27,4 +27,5 @@ curl -s https://api.github.com/repos/Nukesor/pueue/releases/latest \
  | tr -d \" \
  | wget -qi - -O $HOME/.config/systemd/user/pueue.service
 
-mv /tmp/pueue /tmp/pueued ~/bin/
+mv /tmp/pueue ~/bin/
+mv /tmp/pueued /usr/bin/
