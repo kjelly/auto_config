e7ffe3653eed99c71f9878fbc085be0472dd24e3
Kuo-tung Kao <kjellytw@gmail.com>
2024-02-09 12:23:32 +0800

    remove useless code

--- scripts/install-deb-dependency.sh
+++ scripts/install-deb-dependency.sh
@@ -2,8 +2,5 @@
 export LC_ALL=C
 sudo apt update
 sudo apt-get dist-upgrade -y
-sudo apt-get install -y libffi-dev libssl-dev python3-setuptools curl
-curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
-sudo python3 /tmp/get-pip.py
-pip install ansible
+sudo apt-get install -y libffi-dev libssl-dev python3-setuptools curl ansible
 
